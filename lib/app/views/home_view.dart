018812e6b99528f35b177c3da844652a2436734b
1pedr1n <pedrinfnn@gmail.com>
2023-01-31 14:56:40 -0300

    :fire:

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -8,8 +8,20 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
-        Image.network(
-            'https://st2.depositphotos.com/1185628/11062/v/600/depositphotos_110627010-stock-illustration-currency-converter-exchange-icon.jpg')
+        Container(
+          height: 200,
+          width: double.infinity,
+          margin: EdgeInsets.only(top: 50),
+          child: Image.network(
+              'https://st2.depositphotos.com/1185628/11062/v/600/depositphotos_110627010-stock-illustration-currency-converter-exchange-icon.jpg'),
+        ),
+        Row(
+          children: [
+            DropdownButton(items: [], onChanged: (value) {}),
+            TextField(),
+            ElevatedButton(onPressed: () {}, child: Text('Converter'))
+          ],
+        )
       ]),
     );
   }
